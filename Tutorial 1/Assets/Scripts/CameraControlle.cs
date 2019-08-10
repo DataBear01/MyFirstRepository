8a382ee6decb97193ac2eee406e903a953235f19
BBP_Atlas <nbwilliams360@gmail.com>
2019-08-10 12:09:21 -0400

    The Build Is Windows Stand Alone and the default iteration of the project

---
+++ Tutorial 1/Assets/Scripts/CameraControlle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlle : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    void LateUpdate()
+    {
+        // runs everyframe but after everything has been calculated 
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Tutorial 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text CountText;
+    public Text WinText; 
+
+    public Rigidbody rd;
+    private int count; 
+
+    private void Start()
+    {
+        rd = GetComponent<Rigidbody>();
+
+        count = 0;
+        SetCountText();
+    }
+    //This applies for before any of the physics 
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical); 
+
+        rd.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText(); 
+        }
+    }
+
+    void SetCountText ()
+    {
+        CountText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "You Win";
+        }
+    }
+
+    
+}
+
+
